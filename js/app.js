d644242004d21338a92cbb17e4a74805abf9640c
Tonynganga <antonynganga247@gmail.com>
2022-07-26 16:03:44 +0300

    Tuesdays commits

--- js/app.js
+++ js/app.js
@@ -1,67 +1,50 @@
-interface ITodo{
-    name: string;
-    date: string;
-    description: string;
-    completed: boolean;
-}
-
+"use strict";
 // class implementing the interface
-class Todo implements ITodo{
-    constructor(public name: string, public date: string, public description: string, public completed: boolean){}
+class Todo {
+    constructor(name, date, description, completed) {
+        this.name = name;
+        this.date = date;
+        this.description = description;
+        this.completed = completed;
+    }
 }
-
 // class which contains lists of todos and the actions
-class TodoList{
-    public static uncompletedTodos: Todo[] = new Array();
-
-    public static completedTodos: Todo[] = new Array();
-
+class TodoList {
     // create a new todo
-    createTodoItem(name: string, date: string, description: string):number{
+    createTodoItem(name, date, description) {
         let newItem = new Todo(name, date, description, false);
         // console.log(newItem);
-        let totalCount : number = TodoList.uncompletedTodos.push(newItem);
+        let totalCount = TodoList.uncompletedTodos.push(newItem);
         // console.log();
         console.log(TodoList.uncompletedTodos);
-        
-        
-                
         return totalCount;
     }
-
     //return all Todos
-    allTodosItems(): Todo[]{
+    allTodosItems() {
         return TodoList.uncompletedTodos;
     }
 }
-
-window.onload = function(){
-    let task = <HTMLInputElement>document.getElementById("todoName");
-    let date = <HTMLInputElement>document.getElementById("todoDate");
-    let description = <HTMLInputElement>document.getElementById("todoDescription");
-
+TodoList.uncompletedTodos = new Array();
+TodoList.completedTodos = new Array();
+window.onload = function () {
+    var _a;
+    let task = document.getElementById("todoName");
+    let date = document.getElementById("todoDate");
+    let description = document.getElementById("todoDescription");
     // add an event listener for add click
-    document.getElementById("add")?.addEventListener("click", ()=>toAllTasks(task.value, date.value, description.value));
-
-}
-function toAllTasks(task: string, date: string, description: string){
-
+    (_a = document.getElementById("add")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", () => toAllTasks(task.value, date.value, description.value));
+};
+function toAllTasks(task, date, description) {
     let todo = new TodoList();
     // add tasks to the list
-
     todo.createTodoItem(task, date, description);
-
     // console.log(todo);
-    
-
     // Fetched the updated list and create a list item for UI
-    let div = <HTMLDivElement>document.getElementById("todoList");
+    let div = document.getElementById("todoList");
     let list = "<dl class='dl-horizontal'>";
-
-    for(let index=0; index <TodoList.uncompletedTodos.length; index++) {
+    for (let index = 0; index < TodoList.uncompletedTodos.length; index++) {
         // list = list + "<dt> " + TodoList.allTodos[index].name + "</dt> <dd>" + 
         // TodoList.allTodos[index].description + "</dd>" + "<dd> " + TodoList.allTodos[index].date + "</dd>";
-
         list = list + `
         <div class="task-holder">
                 <input type="checkbox" onclick="taskComplete(this);" class="check">
@@ -75,23 +58,15 @@ function toAllTasks(task: string, date: string, description: string){
                     <img src="images/trash-solid.svg" class="trash" onclick="" alt="">
                 </div>
         </div>`;
-
-
-
-
-
     }
     div.innerHTML = list;
-
     // casting
-    (<HTMLInputElement>document.getElementById("todoName")).value = "";
-    (<HTMLInputElement>document.getElementById("todoDescription")).value = "";
+    document.getElementById("todoName").value = "";
+    document.getElementById("todoDescription").value = "";
 }
-
 // Completed tasks
 // class Completed {
 //     // function markCompleted(index){
-
 //     // }
 //     markCompleted(index){
 //         const objIndex = 
