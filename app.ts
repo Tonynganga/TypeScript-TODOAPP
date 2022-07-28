faf0c289a3be830e5d4ec27e198b608523e658a0
Tonynganga <antonynganga247@gmail.com>
2022-07-28 17:32:07 +0300

    Thursdays commits

--- app.ts
+++ app.ts
@@ -1,9 +1,11 @@
+
 interface ITodo{
     name: string;
     date: string;
     description: string;
     completed: boolean;
 }
+
 
 // class implementing the interface
 class Todo implements ITodo{
@@ -16,16 +18,14 @@ class TodoList{
 
     public static completedTodos: Todo[] = new Array();
 
+    
     // create a new todo
     createTodoItem(name: string, date: string, description: string):number{
         let newItem = new Todo(name, date, description, false);
         // console.log(newItem);
         let totalCount : number = TodoList.uncompletedTodos.push(newItem);
         // console.log();
-        console.log(TodoList.uncompletedTodos);
-        
-        
-                
+        console.log( TodoList.uncompletedTodos);        
         return totalCount;
     }
 
@@ -39,12 +39,32 @@ window.onload = function(){
     let task = <HTMLInputElement>document.getElementById("todoName");
     let date = <HTMLInputElement>document.getElementById("todoDate");
     let description = <HTMLInputElement>document.getElementById("todoDescription");
+    let form = <HTMLFormElement>document.getElementById("form");
+    let msg = <HTMLDivElement>document.getElementById("msg");
 
-    // add an event listener for add click
-    document.getElementById("add")?.addEventListener("click", ()=>toAllTasks(task.value, date.value, description.value));
+    form.addEventListener("submit", (e) => {
+        formValidation();
+        e.preventDefault();
+        
+    });
 
+    let formValidation = () => {
+        if (task.value === "" ) {
+            msg.innerHTML = "Task Name cannot be blank!!";
+        }else if(date.value === ""){
+            msg.innerHTML = "Task Date cannot be empty!!";
+        }else if(description.value === ""){
+            msg.innerHTML = "Task Description cannot be empty!!";
+        }
+        else{
+            msg.innerHTML = "";
+            toAllTasks(task.value, date.value, description.value);     
+        }
+    };
+    
 }
-function toAllTasks(task: string, date: string, description: string){
+
+function toAllTasks(this: any, task: string, date: string, description: string){
 
     let todo = new TodoList();
     // add tasks to the list
@@ -52,16 +72,53 @@ function toAllTasks(task: string, date: string, description: string){
     todo.createTodoItem(task, date, description);
 
     // console.log(todo);
-    
+    renderUncompleted();
 
-    // Fetched the updated list and create a list item for UI
+    // casting
+    (<HTMLInputElement>document.getElementById("todoName")).value = "";
+    (<HTMLInputElement>document.getElementById("todoDate")).value = "";
+    (<HTMLInputElement>document.getElementById("todoDescription")).value = "";
+
+}
+
+function Complete(index: number){
+    var taskItem = TodoList.uncompletedTodos[index];
+    TodoList.uncompletedTodos.splice(index, 1);
+    let secondarray: number = TodoList.completedTodos.push(taskItem);
+    renderUncompleted();
+    renderCompleted();
+    return secondarray;
+    
+}
+function renderCompleted(){
+    
+    let div3 = <HTMLDivElement>document.getElementById("todoList2");
+    let list3 = "<dl class='dl-horizontal'>";
+
+    for(let index=0; index <TodoList.completedTodos.length; index++) {
+        console.log("Console name" + TodoList.completedTodos[index].name);
+                
+        list3 = list3 + `
+        <div class="task-holder">
+                <input type="checkbox" onclick="taskComplete(this);" class="check">
+                <div class="upper">
+                    <input type="text" value="${TodoList.completedTodos[index].name}" class="task">
+                    <input type="date" value="${TodoList.completedTodos[index].date}" class="task">
+                </div>
+                <div class="content">
+                    <input type="text" value="${TodoList.completedTodos[index].description}" class="task1">
+                    <img src="images/trash-solid.svg" class="trash" onclick="deleteTask(${index})"  alt="">
+                </div>
+        </div>`;
+    }
+    div3.innerHTML = list3;
+    
+}
+function renderUncompleted(){
     let div = <HTMLDivElement>document.getElementById("todoList");
     let list = "<dl class='dl-horizontal'>";
 
     for(let index=0; index <TodoList.uncompletedTodos.length; index++) {
-        // list = list + "<dt> " + TodoList.allTodos[index].name + "</dt> <dd>" + 
-        // TodoList.allTodos[index].description + "</dd>" + "<dd> " + TodoList.allTodos[index].date + "</dd>";
-
         list = list + `
         <div class="task-holder">
                 <input type="checkbox" onclick="taskComplete(this);" class="check">
@@ -71,8 +128,8 @@ function toAllTasks(task: string, date: string, description: string){
                 </div>
                 <div class="content">
                     <input type="text" value="${TodoList.uncompletedTodos[index].description}" class="task1">
-                    <img src="images/circle-check-solid.svg" class="trash" onclick="" alt="">
-                    <img src="images/trash-solid.svg" class="trash" onclick="" alt="">
+                    <img src="images/circle-check-solid.svg" class="trash" onclick="Complete(${index})"  alt="">
+                    <img src="images/trash-solid.svg" class="trash" onclick="deleteTaskU(${index})" alt="">
                 </div>
         </div>`;
 
@@ -83,17 +140,16 @@ function toAllTasks(task: string, date: string, description: string){
     }
     div.innerHTML = list;
 
-    // casting
-    (<HTMLInputElement>document.getElementById("todoName")).value = "";
-    (<HTMLInputElement>document.getElementById("todoDescription")).value = "";
 }
+function deleteTask(index:number){
+    TodoList.completedTodos.splice(index, 1);
+    console.log(TodoList.completedTodos);
+    renderCompleted();
+}
+function deleteTaskU(index: number){
+    TodoList.uncompletedTodos.splice(index, 1);
+    console.log("This" + TodoList.uncompletedTodos);
 
-// Completed tasks
-// class Completed {
-//     // function markCompleted(index){
-
-//     // }
-//     markCompleted(index){
-//         const objIndex = 
-//     }
-// }
+    renderUncompleted();
+    
+}
--- js/app.js
+++ js/app.js
@@ -27,24 +27,74 @@ class TodoList {
 TodoList.uncompletedTodos = new Array();
 TodoList.completedTodos = new Array();
 window.onload = function () {
-    var _a;
     let task = document.getElementById("todoName");
     let date = document.getElementById("todoDate");
     let description = document.getElementById("todoDescription");
-    // add an event listener for add click
-    (_a = document.getElementById("add")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", () => toAllTasks(task.value, date.value, description.value));
+    let form = document.getElementById("form");
+    let msg = document.getElementById("msg");
+    form.addEventListener("submit", (e) => {
+        formValidation();
+        e.preventDefault();
+    });
+    let formValidation = () => {
+        if (task.value === "") {
+            msg.innerHTML = "Task Name cannot be blank!!";
+        }
+        else if (date.value === "") {
+            msg.innerHTML = "Task Date cannot be empty!!";
+        }
+        else if (description.value === "") {
+            msg.innerHTML = "Task Description cannot be empty!!";
+        }
+        else {
+            msg.innerHTML = "";
+            toAllTasks(task.value, date.value, description.value);
+        }
+    };
 };
 function toAllTasks(task, date, description) {
     let todo = new TodoList();
     // add tasks to the list
     todo.createTodoItem(task, date, description);
     // console.log(todo);
-    // Fetched the updated list and create a list item for UI
+    renderUncompleted();
+    // casting
+    document.getElementById("todoName").value = "";
+    document.getElementById("todoDate").value = "";
+    document.getElementById("todoDescription").value = "";
+}
+function Complete(index) {
+    var taskItem = TodoList.uncompletedTodos[index];
+    TodoList.uncompletedTodos.splice(index, 1);
+    let secondarray = TodoList.completedTodos.push(taskItem);
+    renderUncompleted();
+    renderCompleted();
+    return secondarray;
+}
+function renderCompleted() {
+    let div3 = document.getElementById("todoList2");
+    let list3 = "<dl class='dl-horizontal'>";
+    for (let index = 0; index < TodoList.completedTodos.length; index++) {
+        console.log("Console name" + TodoList.completedTodos[index].name);
+        list3 = list3 + `
+        <div class="task-holder">
+                <input type="checkbox" onclick="taskComplete(this);" class="check">
+                <div class="upper">
+                    <input type="text" value="${TodoList.completedTodos[index].name}" class="task">
+                    <input type="date" value="${TodoList.completedTodos[index].date}" class="task">
+                </div>
+                <div class="content">
+                    <input type="text" value="${TodoList.completedTodos[index].description}" class="task1">
+                    <img src="images/trash-solid.svg" class="trash" onclick="deleteTask(${index})"  alt="">
+                </div>
+        </div>`;
+    }
+    div3.innerHTML = list3;
+}
+function renderUncompleted() {
     let div = document.getElementById("todoList");
     let list = "<dl class='dl-horizontal'>";
     for (let index = 0; index < TodoList.uncompletedTodos.length; index++) {
-        // list = list + "<dt> " + TodoList.allTodos[index].name + "</dt> <dd>" + 
-        // TodoList.allTodos[index].description + "</dd>" + "<dd> " + TodoList.allTodos[index].date + "</dd>";
         list = list + `
         <div class="task-holder">
                 <input type="checkbox" onclick="taskComplete(this);" class="check">
@@ -54,21 +104,20 @@ function toAllTasks(task, date, description) {
                 </div>
                 <div class="content">
                     <input type="text" value="${TodoList.uncompletedTodos[index].description}" class="task1">
-                    <img src="images/circle-check-solid.svg" class="trash" onclick="" alt="">
-                    <img src="images/trash-solid.svg" class="trash" onclick="" alt="">
+                    <img src="images/circle-check-solid.svg" class="trash" onclick="Complete(${index})"  alt="">
+                    <img src="images/trash-solid.svg" class="trash" onclick="deleteTaskU(${index})" alt="">
                 </div>
         </div>`;
     }
     div.innerHTML = list;
-    // casting
-    document.getElementById("todoName").value = "";
-    document.getElementById("todoDescription").value = "";
 }
-// Completed tasks
-// class Completed {
-//     // function markCompleted(index){
-//     // }
-//     markCompleted(index){
-//         const objIndex = 
-//     }
-// }
+function deleteTask(index) {
+    TodoList.completedTodos.splice(index, 1);
+    console.log(TodoList.completedTodos);
+    renderCompleted();
+}
+function deleteTaskU(index) {
+    TodoList.uncompletedTodos.splice(index, 1);
+    console.log("This" + TodoList.uncompletedTodos);
+    renderUncompleted();
+}
